d73bce69ff9a15f489677dc3fef8fcb1c608e17d
Mykola Demianko <mykola@demianko.com>
2019-03-16 07:49:13 +0200

    Add the Main class and set up build.gradle

--- src/main/java/com/demianko/ecdsa/Main.java
+++ src/main/java/com/demianko/ecdsa/Main.java
@@ -1,5 +1,14 @@
 package com.demianko.ecdsa;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
 }
